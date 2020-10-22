6a8d18f170e2148091b6a411632d4d84e47e7286
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-22 08:37:07 +0300

    loading the controller from the url

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -14,10 +14,31 @@ class Core {
     protected $params = [];
 
     public function __construct(){
-        $this->getUrl();
+        // print_r( $this->getUrl());
+        $url = $this->getUrl();
+        // Look in controllers for first value
+
+        // define the location of the file as if it was relative to 
+        // public/index.php
+        $pathToController = '../app/controllers/' . ucwords($url[0]) . '.php';
+        if (file_exists($pathToController)) {
+            // If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 Index
+            unset($url[0]);
+        }
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl(){
-        echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
