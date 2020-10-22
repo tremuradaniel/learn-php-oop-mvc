d3ecf14a6486b0547bb6dc609bc82b109ed48735
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-22 08:16:00 +0300

    bootstrap file and Core Class

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -1,1 +1,23 @@
+<?php
+
 // sort of a web.php from Laravel
+
+/*
+* App Core Class 
+* Creates URL & loads core controller 
+* URL FOMRAT - /controller/methods/params
+*/
+
+class Core {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct(){
+        $this->getUrl();
+    }
+
+    public function getUrl(){
+        echo $_GET['url'];
+    }
+}
