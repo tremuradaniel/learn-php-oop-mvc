adb70f169a923d03c52d84484b863a2fe7c76a15
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-25 10:28:59 +0200

    making framework ready for reuse

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,13 +2,10 @@
     class Pages extends Controller {
         public function __construct()
         {
-            $this->postModel = $this->model("Post");
         }
 
         public function index()
         {
-
-            $posts = $this->postModel->getPosts();
 
             $data = [
                 'title' => 'Welcome!',
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,10 +2,4 @@
 
 <h1><?php echo $data['title'] ?></h1>
 
-<h3><?php echo APPROOT; ?></h3>
-<ul>
-    <?php foreach($data['posts'] as $post): ?>
-        <li><?php echo $post->title ?></li>
-    <?php endforeach ?>
-</ul>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
