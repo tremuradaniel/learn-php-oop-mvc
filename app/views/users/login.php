3a41f62501389431901fe8d779664a32fe87b2be
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-25 14:34:12 +0200

    register and login form view

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -2,26 +2,11 @@
     <div class="row">
         <div class="col-md-6 mx-auto">
             <div class="card card-body bg-light mt-5">
-                <h2>Create An Account</h2>
-                <p>Please fill out this form to register with us</p>
+                <h2>Login</h2>
                 <form 
-                    action="<?php echo URLROOT;?>users/register" 
+                    action="<?php echo URLROOT;?>users/login" 
                     method="post"
                 >
-                    <!-- NAME -->
-                    <div class="form-group">
-                        <label for="name">Name: <sup>*</sup></label>
-                        <input type="text" name="name"
-                            class="form-control form-control-lg
-                                <?php echo (!empty($data['name_err'])) 
-                                    ? 'is-invalid' : '' ?>
-                            "
-                            value="<?php $data['name'] ?>""
-                        >
-                        <span class="invalid-feedback">
-                            <?php echo $data['name_err']; ?>
-                        </span>
-                    </div>
                     <!-- EMAIL -->
                     <div class="form-group">
                         <label for="email">Email: <sup>*</sup></label>
@@ -50,33 +35,16 @@
                             <?php echo $data['password_err']; ?>
                         </span>
                     </div>
-                    <!-- CONFIRM PASSWORD -->
-                    <div class="form-group">
-                        <label for="confirm_password"
-                        >ConfirmPassword: <sup>*</sup></label>
-                        <input type="password" name="confirm_password"
-                            class="form-control form-control-lg
-                                <?php 
-                                    echo (!empty($data['confirm_password_err'])) 
-                                    ? 'is-invalid' : '' 
-                                ?>
-                            "
-                            value="<?php $data['confirm_password'] ?>""
-                        >
-                        <span class="invalid-feedback">
-                            <?php echo $data['password_err']; ?>
-                        </span>
-                    </div>
                     <div class="row">
                         <div class="col">
-                            <input type="submit" value="Register"
+                            <input type="submit" value="Login"
                                 class="btn btn-success btn-block"
                             >
                         </div>
                         <div class="col">
-                            <a href="<?php echo URLROOT;?>users/login" 
+                            <a href="<?php echo URLROOT;?>users/register" 
                             class="btn btn-light btn-block"
-                            >Have an account? Login</a>
+                            >Don't have an account? Register</a>
                         </div>
                     </div>
                 </form>
