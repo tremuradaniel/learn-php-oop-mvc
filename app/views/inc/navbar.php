77669236dfdb09cfff623d8738c48a7a2ae59f29
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-25 14:13:38 +0200

    creating the users controller

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -16,11 +16,11 @@
     </ul>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="<?php echo URLROOT; ?>/users/register"
+        <a class="nav-link" href="<?php echo URLROOT; ?>users/register"
         >Register</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+        <a class="nav-link" href="<?php echo URLROOT; ?>users/login">Login</a>
       </li>
     </ul>
   </div>
