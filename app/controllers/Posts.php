676d29942c5cacb2b7b322196685f86b86f9a446
Daniel Tremura <tremuradaniel@gmail.com>
2020-10-27 07:35:40 +0200

    inserting posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -23,10 +23,47 @@
 
         public function add()
         {
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
-            $this->view('posts/add', $data);
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Sanitize post
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                // Validate title
+                if(empty($data['title'])) {
+                    $data['title_err'] = 'Please enter title';
+                }
+
+                // Validate body
+                if(empty($data['body'])) {
+                    $data['body_err'] = 'Please enter body';
+                }
+
+                 // Make sure errors are empty
+                 if(empty($data['title_err']) && empty($data['body_err']) ) {
+                    // Validated
+                    if($this->postModel->addPost($data)) {
+                        flash('post_message', 'Post added!');
+                        redirect('/posts');
+                    } else {
+                        die('Something went wrong!');
+                    }
+                } else {
+                    // Load views with errors
+                    $this->view('posts/add', $data);
+                }
+
+            } else {
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+                $this->view('posts/add', $data);
+            }
         }
     }
--- app/models/Post.php
+++ app/models/Post.php
@@ -20,4 +20,18 @@
             $results = $this->db->resultSet();
             return $results;
         }
+
+        public function addPost(Array $data)
+        {
+            $sql = 'INSERT INTO posts (title, body, user_id) 
+                    VALUES (:title, :body, :user_id)';
+            $this->db->query($sql);
+            $this->db->bind(':title', $data['title']);
+            $this->db->bind(':body', $data['body']);
+            $this->db->bind(':user_id', $data['user_id']);
+            // Execute
+            if($this->db->execute()) {
+                return true;
+            } else return false;
+        }
     }
